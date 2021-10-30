14863283b38a7dea96f62f4ccef7a8572e5a3a56
Carlos Andres Pasten Bravo <carlos92.pastenbravo@gmail.com>
2021-10-30 17:19:08 -0300

    with search character

--- src/componets/Characters.jsx
+++ src/componets/Characters.jsx
@@ -1,12 +1,58 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, useReducer, useMemo } from 'react';
 import ThemeContext from '../context/ThemeContext';
-import '../styles/Characters.css'
+import '../styles/Characters.css';
+
+const initialState = {
+    favorites: []
+  }
+  
+  const favoriteReducer = (state, action) => {
+    switch (action.type) {
+      case 'ADD_TO_FAVORITE':
+        return {
+          ...state,
+          favorites: [...state.favorites, action.payload]
+        };
+      default:
+        return state;
+    }
+  }
+  
+
+
 
 const Characters = () => {
 
     const { theme } = useContext(ThemeContext);
 
     const [characters, setCharacters] = useState([]);
+
+    // -------------------------------
+
+    const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
+
+    const handleFavorite = favorite => {
+        dispatch({ type: 'ADD_TO_FAVORITE', payload: favorite })
+      }
+    
+
+    // ---------------------------
+
+    const [search, setSearch] = useState('');
+
+    const handleSearch = (event) => {
+        setSearch(event.target.value);
+    }
+
+    // const filteredCharacters = characters.filter((character) => {
+    //     return character.name.toLowerCase().includes(search.toLowerCase());
+    // });
+
+    const filteredCharacters = useMemo(() => 
+        characters.filter((character) =>{
+            return character.name.toLowerCase().includes(search.toLowerCase());
+        }), [search, characters]
+    );
 
     useEffect( () => { // se puede usar useEffect para hacer request a una api 
             fetch(`https://rickandmortyapi.com/api/character/`)
@@ -19,25 +65,44 @@ const Characters = () => {
 
     return (
         <div className='charactersContainer'>
-            {console.log(characters)}
-            {characters.map(character => {
-
-                // { `${theme ? 'dark-information' : ''}` }
-                return  <div id="characterCard" className={`characterCard `+`${theme ? 'darkCard' : ''}` }  key={character.id}>
-                            <div className='characterImage'>
-                                <img src={character.image} alt={character.name} />    
-                            </div>
-                            <h2 className='characterTitle'> {character.name} </h2>
-                            <div className='characterInformation '>
-                                <p> context:  </p>
-                                <p> status : { character.status} </p>
-                                <p> specie : { character.species} </p>
-                                <p> gender : { character.gender} </p>
-                                <p> origin : { character.origin.name } </p>
-                            </div>
-                        </div>
+            <div className='favoritesContainer'>
+                {favorites.favorites.map(favorite => (
+                    <div className="favoriteCard" key={favorite.id}>
+                        <img src={favorite.image} alt={favorite.name} />
+                        {favorite.name}
+                    </div>
                 
-            })}
+                ))}
+            </div>
+           
+            <input className='searchCharacter' placeholder='Search by name' type='text' value={search} onChange={handleSearch} /> 
+                { // si el evento tiene un value, no se puede usar el onChange con una funcion anonima 
+                }
+
+
+            {filteredCharacters.map(character => (
+                <div id="characterCard" className={`characterCard ` + `${theme ? 'darkCard' : ''}`} key={character.id}>
+
+                    <div className='characterImage'>
+                        <img src={character.image} alt={character.name} />
+                        <span
+                            onClick={() => handleFavorite(character)}
+                            className={`favoriteButton material-icons-round ` + `${theme ? 'darkFavoriteButton' : ''}`}> star_rate
+                        </span>
+                        
+                    </div>
+                    <h2 className='characterTitle'> {character.name} </h2>
+                    <div className='characterInformation '>
+                        <p> context:  </p>
+                        <p> status : {character.status} </p>
+                        <p> specie : {character.species} </p>
+                        <p> gender : {character.gender} </p>
+                        <p> origin : {character.origin.name} </p>
+                    </div>
+                    
+                </div>
+
+            ))}
         </div>
     );
 }
